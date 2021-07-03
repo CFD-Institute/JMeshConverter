135ff9fcd79cd864a09ddfc097e146ea479a3142
DANG Truong <dangtruong080593@gmail.com>
2021-07-03 16:41:08 +0700

    first commit

---
+++ src/com/misacfd/meshconverter/GmshReader.java
@@ -0,0 +1,69 @@
+package com.misacfd.meshconverter;
+
+import java.util.List;
+
+public class GmshReader {
+    private long nbNode;
+    private long nbElMsh;
+    private long nbElm;
+    private String fName;
+    private List<Point> coordNodes;
+    private List<NodeIdent> idNodes;
+    private List<NodeIdentMsh> idNodesMsh;
+
+    public long getNbNode() {
+        return nbNode;
+    }
+
+    public void setNbNode(long nbNode) {
+        this.nbNode = nbNode;
+    }
+
+    public long getNbElMsh() {
+        return nbElMsh;
+    }
+
+    public void setNbElMsh(long nbElMsh) {
+        this.nbElMsh = nbElMsh;
+    }
+
+    public long getNbElm() {
+        return nbElm;
+    }
+
+    public void setNbElm(long nbElm) {
+        this.nbElm = nbElm;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public List<Point> getCoordNodes() {
+        return coordNodes;
+    }
+
+    public void setCoordNodes(List<Point> coordNodes) {
+        this.coordNodes = coordNodes;
+    }
+
+    public List<NodeIdent> getIdNodes() {
+        return idNodes;
+    }
+
+    public void setIdNodes(List<NodeIdent> idNodes) {
+        this.idNodes = idNodes;
+    }
+
+    public List<NodeIdentMsh> getIdNodesMsh() {
+        return idNodesMsh;
+    }
+
+    public void setIdNodesMsh(List<NodeIdentMsh> idNodesMsh) {
+        this.idNodesMsh = idNodesMsh;
+    }
+}
